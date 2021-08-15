afb8e6b85828fcfae02414b4e9cc2822241bdb6a
Douglas-Cezaro <cezarodouglas@gmail.com>
2021-08-15 20:48:48 -0300

    Finished class Testes automatizados

--- c-plus-plus-tdd-testes-unidade-catch2/Leilao/Leilao/teste-avaliador.cpp
+++ c-plus-plus-tdd-testes-unidade-catch2/Leilao/Leilao/teste-avaliador.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 
 int main() {
-
+	
+	// Arrange - Given
 	Lance primeiroLance(Usuario("Douglas"), 1000);
 	Lance segundoLance(Usuario("Ana Maria"), 2000);
 	Leilao leilao("Fiat 147 0Km");
@@ -10,9 +11,19 @@ int main() {
 	leilao.recebeLance(primeiroLance);
 
 	Avaliador leiloeiro;
+
+	// Act - When
 	leiloeiro.avalia(leilao);
 
-	std::cout << leiloeiro.recuperarMaiorValor() << std::endl;
+	// Assert - Then
+	float valorEsperado = 2000;
+		
+	if (valorEsperado == leiloeiro.recuperarMaiorValor()) {
+		std::cout << "TESTE OK" << std::endl;
+	}
+	else {
+		std::cout << "TESTE FALHOU" << std::endl;
+	}
 
 	return 0;
 }
